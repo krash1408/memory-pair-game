c88f0ba8bee879213d8e18c33e0fdfe722393e92
Konstantin Makarevich <krash1408@gmail.com>
2019-01-08 21:10:11 +0300

    Initialising project. Interface creating.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,49 @@ let field = document.querySelector('.game-field');
 let wrapper = document.createElement('div');
 wrapper.classList.add('game-field__wrapper');
 
-createField = (x, y) = {
-    
+field.appendChild(wrapper);
+createCard = () => {
+    let tmpArr = [];
+    for (var i = 0; i < 8; i++) {
+        const card = document.createElement('div');
+        const cardWrapper = document.createElement('div');
+        const cardFront = document.createElement('div');
+        const cardBack = document.createElement('div');
+        card.classList.add('card');
+        cardWrapper.classList.add('card__wrapper');
+        cardFront.classList.add('card__front');
+        cardBack.classList.add('card__back');
+        cardBack.innerHTML = i + 1;
+        card.appendChild(cardWrapper);
+        cardWrapper.appendChild(cardFront);
+        cardWrapper.appendChild(cardBack);
+        tmpArr.push(card);
+    }
+    return tmpArr;
 }
-// field.appendChild(wrapper);
+randomize = () => {
+    return createCard().sort(function() { return 0.5 - Math.random() });
+}
+
+randomize().forEach(val => {
+    wrapper.appendChild(val);
+});
+randomize().forEach(val => {
+    wrapper.appendChild(val);
+});
+
+cardArr = document.querySelectorAll('.card__wrapper')
+cardArr.forEach(value => {
+    value.addEventListener('click', (evt) => {
+        value.classList.toggle('card__wrapper--click');
+    })
+})
+
+createPool = () => {
+    let arr = [];
+    if (arr.length > 1) {
+        return;
+    } else {
+        
+    }
+}
