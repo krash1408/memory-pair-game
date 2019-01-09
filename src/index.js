f2c8f2f6dccd7411cd272c17bcb5d0bebdf051a1
Konstantin Makarevich <krash1408@gmail.com>
2019-01-09 13:52:40 +0200

    Create a shuffle function, add start button activities

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 
-let field = document.querySelector('.game-field');
-let content = [
-    'img/fb.svg',
-    'img/line.svg'
-]
-
+const field = document.querySelector('.game-field');
 let wrapper = document.createElement('div');
 wrapper.classList.add('game-field__wrapper');
-
 field.appendChild(wrapper);
-createCard = () => {
+
+const imageArray = [
+        '../img/fb.svg',
+        '../img/fb.svg',
+        '../img/line.svg',
+        '../img/line.svg',
+        '../img/skype.svg',
+        '../img/skype.svg',
+        '../img/snap.svg',
+        '../img/snap.svg',
+        '../img/tlg.svg',
+        '../img/tlg.svg',
+        '../img/viber.svg',
+        '../img/viber.svg',
+        '../img/weixin.svg',
+        '../img/weixin.svg',
+        '../img/whatsapp.svg',
+        '../img/whatsapp.svg'
+    ];
+let shuffle = function(val) {
+    var j, x, i;
+    for (i = val.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = val[i];
+        val[i] = val[j];
+        val[j] = x;
+    }
+    return val;
+}
+
+createCard = (num) => {
     let tmpArr = [];
-    for (var i = 0; i < 16; i++) {
+    for (var i = 0; i < num; i++) {
         const card = document.createElement('div');
         const cardWrapper = document.createElement('div');
         const cardFront = document.createElement('div');
@@ -20,7 +44,6 @@ createCard = () => {
         cardWrapper.classList.add('card__wrapper');
         cardFront.classList.add('card__front');
         cardBack.classList.add('card__back');
-        cardBack.style.backgroundImage = 'img/fb.svg';
         card.appendChild(cardWrapper);
         cardWrapper.appendChild(cardFront);
         cardWrapper.appendChild(cardBack);
@@ -28,17 +51,36 @@ createCard = () => {
     }
     return tmpArr;
 }
-randomize = () => {
-    return createCard().sort(function() { return 0.5 - Math.random() });
-}
-//заменить createCard на randomize
-createCard().forEach(val => {
+
+createCard(16).forEach(val => {
     wrapper.appendChild(val);
 });
 
-cardArr = document.querySelectorAll('.card__wrapper')
-cardArr.forEach(value => {
-    value.addEventListener('click', (evt) => {
-        value.classList.toggle('card__wrapper--click');
-    })
+let cardArr = document.querySelectorAll('.card__wrapper');
+    cardArr.forEach(value => {
+        value.addEventListener('click', () => {
+            value.classList.toggle('card__wrapper--click');
+        })
+    });
+document.querySelector('.start-btn').addEventListener('click', function() {
+    this.classList.add('active');
+    newGame();
 })
+
+document.querySelector('.restart-btn').addEventListener('click', function() {
+    document.querySelector('.start-btn').classList.remove('active');
+
+})
+
+function newGame() {
+    cardBackArr = $('.card__back').toArray();
+    cardBackArr.forEach((val, ind) => {
+        let shuffledArray = new shuffle(imageArray);
+        $(document).ready(function () {
+            $(val).css('background-image', `url(${shuffledArray[ind]})`);
+            $(val).css('background-repeat', 'no-repeat');
+            $(val).css('background-position', 'center center');
+        });
+    });
+}
+
