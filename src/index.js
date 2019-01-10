b5f4a3d2139054f3f987e913063efbda7f9464c7
Konstantin Makarevich <krash1408@gmail.com>
2019-01-10 17:09:35 +0200

    RC 1.0

--- src/index.js
+++ src/index.js
@@ -1,85 +1,136 @@
-const field = document.querySelector('.game-field');
-let wrapper = document.createElement('div');
-wrapper.classList.add('game-field__wrapper');
-field.appendChild(wrapper);
 
-const imageArray = [
-        '../img/fb.svg',
-        '../img/fb.svg',
-        '../img/line.svg',
-        '../img/line.svg',
-        '../img/skype.svg',
-        '../img/skype.svg',
-        '../img/snap.svg',
-        '../img/snap.svg',
-        '../img/tlg.svg',
-        '../img/tlg.svg',
-        '../img/viber.svg',
-        '../img/viber.svg',
-        '../img/weixin.svg',
-        '../img/weixin.svg',
-        '../img/whatsapp.svg',
-        '../img/whatsapp.svg'
-    ];
-let shuffle = function(val) {
-    var j, x, i;
-    for (i = val.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = val[i];
-        val[i] = val[j];
-        val[j] = x;
-    }
-    return val;
-}
-
-createCard = (num) => {
+  wrapper = document.querySelector('.game-field__wrapper');
+  let cardsPool = [];
+  let openedCards = [];
+  const imageArray = [
+    { 'id' : 'fb',       'img' : '../img/fb.svg'},
+    { 'id' : 'line',     'img' : '../img/line.svg'},
+    { 'id' : 'skype',    'img' : '../img/skype.svg'},
+    { 'id' : 'snap',     'img' : '../img/snap.svg'},
+    { 'id' : 'tlg',      'img' : '../img/tlg.svg'},
+    { 'id' : 'viber',    'img' : '../img/viber.svg'},
+    { 'id' : 'weixin',   'img' : '../img/weixin.svg'},
+    { 'id' : 'whatsapp', 'img' : '../img/whatsapp.svg'}
+  ];
+  const data = [...imageArray, ...imageArray];
+  //создать num-блоков
+  function createCardField (value) {
+    value.forEach( val => {
+      wrapper.appendChild( val );
+    } );
+  }
+  //создать поле из num карточек через fn createCard(num)
+  function createCard ( num ) {
     let tmpArr = [];
     for (var i = 0; i < num; i++) {
-        const card = document.createElement('div');
-        const cardWrapper = document.createElement('div');
-        const cardFront = document.createElement('div');
-        const cardBack = document.createElement('div');
-        card.classList.add('card');
-        cardWrapper.classList.add('card__wrapper');
-        cardFront.classList.add('card__front');
-        cardBack.classList.add('card__back');
-        card.appendChild(cardWrapper);
-        cardWrapper.appendChild(cardFront);
-        cardWrapper.appendChild(cardBack);
-        tmpArr.push(card);
+      const card = document.createElement('div');
+      const cardWrapper = document.createElement('div');
+      const cardFront = document.createElement('div');
+      const cardBack = document.createElement('div');
+      card.classList.add('card');
+      cardWrapper.classList.add('card__wrapper');
+      cardFront.classList.add('card__front');
+      cardBack.classList.add('card__back');
+      card.appendChild(cardWrapper);
+      cardWrapper.appendChild(cardFront);
+      cardWrapper.appendChild(cardBack);
+      tmpArr.push(card);
     }
     return tmpArr;
-}
 
-createCard(16).forEach(val => {
-    wrapper.appendChild(val);
-});
+  }
+  //перемешать массив
+  function shuffleImages () {
 
-let cardArr = document.querySelectorAll('.card__wrapper');
-    cardArr.forEach(value => {
-        value.addEventListener('click', () => {
-            value.classList.toggle('card__wrapper--click');
-        })
-    });
-document.querySelector('.start-btn').addEventListener('click', function() {
-    this.classList.add('active');
-    newGame();
-})
-
-document.querySelector('.restart-btn').addEventListener('click', function() {
-    document.querySelector('.start-btn').classList.remove('active');
-
-})
-
-function newGame() {
+    var j, x, i;
+    for (i = data.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = data[i];
+      data[i] = data[j];
+      data[j] = x;
+    }
+    return data;
+  }
+  // наполнить блоки 
+  function insertContentInCards() {
     cardBackArr = $('.card__back').toArray();
+    let shuffledArray = new shuffleImages();
     cardBackArr.forEach((val, ind) => {
-        let shuffledArray = new shuffle(imageArray);
-        $(document).ready(function () {
-            $(val).css('background-image', `url(${shuffledArray[ind]})`);
-            $(val).css('background-repeat', 'no-repeat');
-            $(val).css('background-position', 'center center');
-        });
+      $(document).ready(function () {
+        $(val).css('background-image', `url(${shuffledArray[ind].img})`);
+        $(val).css('background-repeat', 'no-repeat');
+        $(val).css('background-position', 'center center');
+        val.setAttribute('data-name', `${shuffledArray[ind].id}`)
+      });
     });
-}
+  }
 
+function gameRules(val) {
+  const cardFrontArray = $('.card__front').toArray();
+  if (cardFrontArray.includes(val.originalTarget) !== true) {
+    console.log(false);
+  }
+  else {
+    val.originalTarget.offsetParent.classList.toggle('card__wrapper--click');
+    cardsPool.push(event.target.nextSibling);
+    if (cardsPool.length === 2 && checkCardsPool(cardsPool)) {
+      cardsPool.forEach(val => {
+        val.offsetParent.offsetParent.classList.add('same-cards');
+        openedCards.push(val.offsetParent.offsetParent);
+      });
+      cardsPool = [];
+      
+    }
+    else if (cardsPool.length > 1 && checkCardsPool(cardsPool) !== true) {
+      cardsPool.forEach(value => {
+        setTimeout(() => {
+          value.offsetParent.classList.remove('card__wrapper--click');
+        }, 500);
+      });
+      cardsPool = [];
+    }
+  }
+}
+function win() {
+  if (openedCards.length == 16) {
+    alert('You win!');
+  }
+}
+  function help() {
+    setTimeout( () => {
+      $('.card__wrapper').addClass('help-btn--click');
+    }, 500);
+    setTimeout( () => {
+      $('.card__wrapper').removeClass('help-btn--click');
+    }, 2000);
+    console.log('click');
+  }
+
+  function checkCardsPool (arr) {
+    return arr[0].dataset.name === arr[1].dataset.name;
+  }
+
+
+  wrapper.addEventListener( 'click', (val) => {
+    gameRules(val);
+    win();
+  } );
+
+  $('.help-btn').click( function(){
+    help();
+    return;
+  } );
+
+  $('.start-btn').click(function() {
+    if ( this.classList.contains('active') ) {
+      $('.card').toArray().forEach(val => {
+        wrapper.removeChild(val);
+      });
+    }
+    this.classList.add('active');
+    setTimeout(()=>{
+      help();
+      createCardField(createCard(16));
+      insertContentInCards();
+    },500);
+  });
